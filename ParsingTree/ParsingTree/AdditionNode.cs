8acedab9a8f4e404bc5deb322f73fc7deaa452f1
Andrew Kalinin <Fipett2407@gmail.com>
2025-04-04 23:59:52 +0300

    add: the forgotten module

---
+++ ParsingTree/ParsingTree/AdditionNode.cs
@@ -0,0 +1,28 @@
+﻿// <copyright file="AdditionNode.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace ParsingTree
+{
+    public class AdditionNode : Node
+    {
+        private Node left;
+        private Node right;
+        public AdditionNode(Node left, Node right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override int ValueOrExpression() => left.ValueOrExpression() + right.ValueOrExpression();
+
+        public override void Print()
+        {
+            Console.Write("(+ ");
+            this.left.Print();
+            Console.Write(" ");
+            this.right.Print();
+            Console.Write(")");
+        }
+    }
+}
