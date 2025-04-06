2125e7535b9eedc3b9f38d53d9e73e3d463acf34
Andrew Kalinin <Fipett2407@gmail.com>
2025-04-06 04:33:54 +0300

    test: removed warnings related to comments for tests

--- ParsingTree/ParsingTree.Tests/ParserTests.cs
+++ ParsingTree/ParsingTree.Tests/ParserTests.cs
@@ -1,47 +1,56 @@
-﻿namespace ParsingTree.Tests
+﻿// <copyright file="ParserTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+#pragma warning disable SA1600
+
+namespace ParsingTree.Tests
 {
-    public class Tests
+    /// <summary>
+    /// Class with tests from parser.
+    /// </summary>
+    public class ParserTests
     {
         private Parser parser;
 
         [SetUp]
         public void SetUp()
         {
-            parser = new Parser();
+            this.parser = new Parser();
         }
 
         [Test]
         public void Parse_SimpleExpression_ShouldReturnCorrectValue()
         {
-            var root = parser.Parse("(+ 3 8)");
+            var root = this.parser.Parse("(+ 3 8)");
             Assert.That(root.ValueOrExpression, Is.EqualTo(11));
         }
 
         [Test]
         public void Parse_ComplexExpression_ShouldReturnCorrectValue()
         {
-            var root = parser.Parse("(* (+ 3 8) (/ 18 6) ) ");
+            var root = this.parser.Parse("(* (+ 3 8) (/ 18 6) ) ");
             Assert.That(root.ValueOrExpression, Is.EqualTo(33));
         }
 
         [Test]
         public void Parse_InvalidOperator_ShouldThrowException()
         {
-            var ex = Assert.Throws<InvalidDataException>(() => parser.Parse("($ 1 3"));
+            var ex = Assert.Throws<InvalidDataException>(() => this.parser.Parse("($ 1 3"));
             Assert.That(ex.Message, Does.Contain("Invalid symbol $"));
         }
 
         [Test]
         public void Parse_UnclosedBracket_ShouldThrowException()
         {
-            var ex = Assert.Throws<InvalidDataException>(() => parser.Parse("(/ 6 3"));
+            var ex = Assert.Throws<InvalidDataException>(() => this.parser.Parse("(/ 6 3"));
             Assert.That(ex.Message, Does.Contain("expected ')'"));
         }
 
         [Test]
         public void Parse_DivisionByZero_ShouldThrowException()
         {
-            var root = parser.Parse("(/ 3 0)");
+            var root = this.parser.Parse("(/ 3 0)");
             var ex = Assert.Throws<DivideByZeroException>(() => root.ValueOrExpression());
             Assert.That(ex.Message, Does.Contain("division by zero"));
         }
@@ -49,9 +58,8 @@
         [Test]
         public void Parse_NegativeNumber_ShouldReturnCorrectValue()
         {
-            var root = parser.Parse("-3");
+            var root = this.parser.Parse("-3");
             Assert.That(root.ValueOrExpression, Is.EqualTo(-3));
         }
     }
 }
-
