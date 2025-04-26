2d8c9abbad344c031f81b53b7516bdc9e972dfb8
Andrew Kalinin <Fipett2407@gmail.com>
2025-04-26 20:03:22 +0300

    check CI working

--- CalculatorOnForms2/CalculatorOnForms2.Tests/CalculatorTests.cs
+++ CalculatorOnForms2/CalculatorOnForms2.Tests/CalculatorTests.cs
@@ -27,6 +27,7 @@
             testCalculator.AppendNumber("5");
             testCalculator.AppendNumber("7");
             Assert.That(testCalculator.CurrentDisplay, Is.EqualTo("157"));
+            Assert.Fail();
         }
 
 
