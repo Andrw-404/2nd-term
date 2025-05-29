ec60ce6a3b495350cab50ad68bdb453c3afcfc8f
Andrew Kalinin <Fipett2407@gmail.com>
2025-05-30 00:42:09 +0300

    test: add tests for string list and empty list

--- FunctionalUtils/FunctionalUtils.Tests/TestsForAllFunctions.cs
+++ FunctionalUtils/FunctionalUtils.Tests/TestsForAllFunctions.cs
@@ -39,4 +39,59 @@ public class TestsForAllFunctions
 
         Assert.That(result, Is.EqualTo(16));
     }
+
+    [Test]
+    public void Map_StringList_ShouldReturnCorrectList()
+    {
+        List<string> strings = new List<string> { "a", "bb", "ccc", "dddd" };
+        List<int> expectedList = new List<int> { 1, 2, 3, 4 };
+        var result = HaveMap.Map(strings, x => x.Length);
+
+        Assert.That(result, Is.EqualTo(expectedList));
+    }
+
+    [Test]
+    public void Filter_StringList_ShouldReturnCorrectList()
+    {
+        List<string> strings = new List<string> { "a", "bb", "ccc", "dddd" };
+        List<string> expectedList = new List<string> { "ccc", "dddd" };
+        var result = HaveFilter.Filter(strings, x => x.Length > 2);
+
+        Assert.That(result, Is.EqualTo(expectedList));
+    }
+
+    [Test]
+    public void Fold_StringList_ShouldReturnCorrectNumber()
+    {
+        List<string> strings = new List<string> { "Hello", "World" };
+        string expected = "Hello-World";
+
+        var result = HaveFold.Fold(strings, string.Empty, (accumulator, element) => accumulator + (accumulator == string.Empty ? string.Empty : "-") + $"{element}");
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Map_EmptyList_ShouldReturnCorrectList()
+    {
+        var result = HaveMap.Map(new List<int>(), x => x * 2);
+
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Filter_EmptyList_ShouldReturnCorrectList()
+    {
+        var result = HaveFilter.Filter(new List<int>(), x => x % 2 == 1);
+
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Fold_EmptyList_ShouldReturnCorrectNumber()
+    {
+        var result = HaveFold.Fold(new List<int>(), 0, (accumulator, number) => accumulator + number);
+
+        Assert.That(result, Is.EqualTo(0));
+    }
 }
