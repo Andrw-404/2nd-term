74e0b0bca93119a547d8ddf6d653568d097d59d8
Andrew Kalinin <Fipett2407@gmail.com>
2025-05-30 02:09:14 +0300

    feat: removed the explicit construction of tables

--- BWT/BWT/BWTAlgorithm.cs
+++ BWT/BWT/BWTAlgorithm.cs
@@ -1,47 +1,106 @@
-﻿namespace BWT
-{
-    using System.Text;
-    public class BWTAlgorithm
-    {
-        public static (string, int) FrontBWT(string input)
-        {
-            int lengthOfString = input.Length;
-            string[] rotations = new string[lengthOfString];
+﻿namespace BWT;
 
+public class BWTAlgorithm
+{
+    public static (string, int) FrontBWT(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return (string.Empty, -1);
+        }
+
+        int lengthOfString = input.Length;
+        int[] indices = new int[lengthOfString];
+
+        for (int i = 0; i < lengthOfString; ++i)
+        {
+            indices[i] = i;
+        }
+
+        Array.Sort(indices, (a, b) =>
+        {
             for (int i = 0; i < lengthOfString; ++i)
             {
-                rotations[i] = input.Substring(i) + input.Substring(0, i);
-            }
-
-            Array.Sort(rotations);
-
-            string transformed = new string(rotations.Select(s => s[lengthOfString - 1]).ToArray());
-            int bwtIndex = Array.IndexOf(rotations, input);
-
-            return (transformed, bwtIndex);
-        }
-
-        public static string InverseBWT(string bwtString, int originalIndex)
-        {
-            int lengthOfBWTString = bwtString.Length;
-
-            string[] table = new string[lengthOfBWTString];
-            for (int i = 0; i < lengthOfBWTString; ++i)
-            {
-                table[i] = string.Empty;
-            }
-
-            for (int q = 0; q < lengthOfBWTString; ++q)
-            {
-                for (int k = 0; k < lengthOfBWTString; ++k)
+                char charA = input[(a + i) % lengthOfString];
+                char charB = input[(b + i) % lengthOfString];
+                if (charA != charB)
                 {
-                    table[k] = bwtString[k] + table[k];
+                    return charA.CompareTo(charB);
                 }
-
-                Array.Sort(table);
             }
 
-            return table[originalIndex];
+            return 0;
+        });
+
+        char[] bwtChars = new char[lengthOfString];
+        int bwtIndex = -1;
+
+        for (int i = 0; i < lengthOfString; ++i)
+        {
+            int rotationsStartIndex = indices[i];
+
+            bwtChars[i] = input[(rotationsStartIndex + lengthOfString - 1) % lengthOfString];
+
+            if (rotationsStartIndex == 0)
+            {
+                bwtIndex = i;
+            }
         }
+
+        string transformedString = new string(bwtChars);
+        return (transformedString, bwtIndex);
+    }
+
+    public static string InverseBWT(string bwtString, int originalIndex)
+    {
+        if (string.IsNullOrEmpty(bwtString) || originalIndex < 0)
+        {
+            return string.Empty;
+        }
+
+        int lengthOfBWTString = bwtString.Length;
+
+        char[] result = new char[lengthOfBWTString];
+        int[] next = new int[lengthOfBWTString];
+
+        Dictionary<char, int> counts = new Dictionary<char, int>();
+        foreach (char symbol in bwtString)
+        {
+            if (counts.ContainsKey(symbol))
+            {
+                counts[symbol]++;
+            }
+            else
+            {
+                counts[symbol] = 1;
+            }
+        }
+
+        var sortedChars = counts.Keys.OrderBy(c => c).ToList();
+
+        Dictionary<char, int> startPos = new Dictionary<char, int>();
+        int pos = 0;
+        foreach (char symbol in sortedChars)
+        {
+            startPos[symbol] = pos;
+            pos += counts[symbol];
+        }
+
+        Dictionary<char, int> tmpCount = new Dictionary<char, int>(startPos);
+        for (int i = 0; i < lengthOfBWTString; ++i)
+        {
+            char symbol = bwtString[i];
+            next[i] = tmpCount[symbol];
+            tmpCount[symbol]++;
+        }
+
+        int current = originalIndex;
+        for (int i = lengthOfBWTString - 1; i >= 0; --i)
+        {
+            result[i] = bwtString[current];
+            current = next[current];
+        }
+
+        return new string(result);
     }
 }
--- BWT/BWT/Tests.cs
+++ BWT/BWT/Tests.cs
@@ -1,43 +1,41 @@
-﻿namespace BWT
+﻿namespace BWT;
+
+public class Tests
 {
-    public class Tests
+    public static bool RunTest()
     {
-        public static bool RunTest()
+        var testArray = new (string name, Func<bool> test)[]
         {
-            var testArray = new (string name, Func<bool> test)[]
-            {
-                ("BWTFrontTest", BWTFrontTest),
-                ("BWTInverseTest", BWTInverseTest),
-            };
+            ("BWTFrontTest", BWTFrontTest),
+            ("BWTInverseTest", BWTInverseTest),
+        };
 
-            foreach (var testFunctions in testArray)
+        foreach (var testFunctions in testArray)
+        {
+            if (!testFunctions.test())
             {
-                if (!testFunctions.test())
-                {
-                    Console.WriteLine($"{testFunctions.name} failed the test");
-                    return false;
-                }
+                Console.WriteLine($"{testFunctions.name} failed the test");
+                return false;
             }
-
-            return true;
         }
 
-        private static bool BWTFrontTest()
-        {
-            var (testString, testIndex) = BWTAlgorithm.FrontBWT("banana");
-            string expectedString = "nnbaaa";
-            int expectedIndex = 3;
-            return testString == expectedString && testIndex == expectedIndex;
-        }
+        return true;
+    }
 
-        private static bool BWTInverseTest()
-        {
-            string testString = "nnbaaa";
-            int testIndex = 3;
-            string testResult = BWTAlgorithm.InverseBWT(testString, testIndex);
-            string expectedString = "banana";
-            return testResult == expectedString;
-        }
+    private static bool BWTFrontTest()
+    {
+        var (testString, testIndex) = BWTAlgorithm.FrontBWT("banana");
+        string expectedString = "nnbaaa";
+        int expectedIndex = 3;
+        return testString == expectedString && testIndex == expectedIndex;
+    }
 
+    private static bool BWTInverseTest()
+    {
+        string testString = "nnbaaa";
+        int testIndex = 3;
+        string testResult = BWTAlgorithm.InverseBWT(testString, testIndex);
+        string expectedString = "banana";
+        return testResult == expectedString;
     }
 }
